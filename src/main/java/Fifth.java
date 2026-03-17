a5fa8a2bb82e60a6c66ae463279d8a9850474af5
Ivan Maslakov <0500456367v@gmail.com>
2026-03-17 20:43:05 +0200

    ASCII sybmols

---
+++ src/main/java/Fifth.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fifth {
+    public static void main() {
+
+        System.out.println("Введіть будь-яке число від 0 до 127:");
+        Scanner number = new Scanner(System.in);
+
+        int number_value = number.nextInt();
+
+        boolean symbol_value = false;
+
+        while (!symbol_value) {
+            if (number_value > 0 && number_value < 127) {
+                char symbol = (char) number_value;
+                System.out.println("Ваше число відповідає символу: " + symbol);
+                symbol_value = true;
+            } else if (number_value < 0 || number_value > 127) {
+                System.out.println("Введіть число у діапазоні від 0 до 127");
+                number_value = number.nextInt();
+            }
+        }
+    }
+}
